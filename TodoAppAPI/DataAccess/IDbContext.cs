001b9b62496a3349b8306902d707f21f4e13756b
Dawid Cyganek <28643223+hhimko@users.noreply.github.com>
2022-08-21 16:06:44 +0200

    Add basic TodoItem retrieval

---
+++ TodoAppAPI/DataAccess/IDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoAppAPI.DataAccess;
+
+
+public interface IDbContext
+{
+    public TodoItem? GetById(long id);
+    public IEnumerable<TodoItem> GetAllByDayNumber(int dayNumber);
+}
---
+++ TodoAppAPI/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using TodoAppAPI.DataAccess.JsonConverters;
+
+namespace TodoAppAPI.Models;
+
+
+public record TodoItem(
+    long Id,
+    string Name,
+    int DayNumber,
+    [field: JsonConverter(typeof(TimeOnlyJsonConverter))] 
+    DateTime? ScheduledTime = null
+)
+{
+    [JsonIgnore]
+    public DateOnly? Date => DateOnly.FromDayNumber(DayNumber);
+}
